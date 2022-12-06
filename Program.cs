822ff08f3e6b41e7be71b0e3b0deb32fdb7fd9d8
SzymonSolomianko <116341751+SzymonSolomianko@users.noreply.github.com>
2022-12-06 14:20:31 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// 1
+Console.WriteLine("PI = {0:#.####}", Math.PI);
+
+// 2
+//Console.WriteLine(Math.Sqrt(Math.PI));
+
+// 3
+
+//using System;
+//class Circle
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Podaj promien: ");
+//        double Radious = Convert.ToDouble(Console.ReadLine());
+//        double Area = Math.PI * Radious * Radious;
+//        Console.WriteLine("Objetosc: " + Area);
+//        Console.ReadKey();
+//    }
+//}
