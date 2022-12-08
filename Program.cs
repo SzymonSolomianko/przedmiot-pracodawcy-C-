a2250f31f9c0659779c8d5697e4b7a44918085fd
SzymonSolomianko <116341751+SzymonSolomianko@users.noreply.github.com>
2022-12-08 14:16:14 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,9 +16,45 @@
 //        Console.WriteLine("Objetosc: " + Area);
 //        Console.ReadKey();
 //    }
-//
-//zad pitagoras
 
+//zad pitagoras
 //class Pitagoras
-Console.Write("Podaj liczbę a:");
-double a =  Convert.ToDouble(Console.ReadLine());
+//Console.Write("Podaj liczbę a:");
+//double a =  Convert.ToDouble(Console.ReadLine());
+
+//zad5
+//Console.WriteLine("Podaj liczbę a");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Podaj liczbę b");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a/b);
+
+//zad6
+//Console.WriteLine("Podaj liczbę x");
+//int x = int.Parse(Console.ReadLine());
+//Console.WriteLine("Podaj liczbę y");
+//int y = int.Parse(Console.ReadLine());
+//Console.WriteLine("wynik dodawania : {0}", x + y);
+//Console.WriteLine("wynik odejmowania : {0}", x - y);
+//Console.WriteLine("wynik mnożenia : {0}", x * y);
+//Console.WriteLine("wynik dzielenia : {0}", x / y);
+
+//zad7
+class zadanie7
+{
+    static void Main(string[]args)
+    {
+        int x = 0,  y = 0;
+        Console.WriteLine("x od 0 do 10");
+        Console.WriteLine();
+        Console.Write("y od 0 do 30");
+        Console.WriteLine();
+        for (x = 0; x <= 10; x++)
+        {
+            y = 3 * x;
+            Console.WriteLine("x=" + x + 't' + "y=" + y);
+
+        }
+    }    
+    
+}
