481b5146b4c9252741f751f2bf843a6bf934802d
SzymonSolomianko <116341751+SzymonSolomianko@users.noreply.github.com>
2022-12-15 18:49:16 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,22 +39,124 @@
 //Console.WriteLine("wynik mnożenia : {0}", x * y);
 //Console.WriteLine("wynik dzielenia : {0}", x / y);
 
-//zad7
-class zadanie7
-{
-    static void Main(string[]args)
-    {
-        int x = 0,  y = 0;
-        Console.WriteLine("x od 0 do 10");
-        Console.WriteLine();
-        Console.Write("y od 0 do 30");
-        Console.WriteLine();
-        for (x = 0; x <= 10; x++)
-        {
-            y = 3 * x;
-            Console.WriteLine("x=" + x + 't' + "y=" + y);
+//zad dodatkowe1
 
-        }
-    }    
-    
+//int sum = 0;
+//int i = 1;
+//while (i <= 100)
+//{
+//  sum += i;
+//i++;
+//}
+
+//Console.WriteLine(sum);
+
+//zad dodatkowe2
+//int sum = 0;
+
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//Zad1.
+//Napisz program, który za pomocą instrukcji for dla danych wartości x zmieniających się w
+//przedziale od 0 do 10 obliczy wartość funkcji y = 3x.
+//int x, y ;
+//for (x = 0; x <11; x++)
+//{
+//    y = 3*x;
+//    Console.WriteLine("x="+x+" y="+y);
+
+//}
+//Zad2.
+//Napisz program, który za pomocą instrukcji do ... while dla danych
+//wartości x zmieniających się od 0 do 10 oblicza wartość funkcji y = 3x
+
+//int x=0, y=0;
+//do
+//{
+//    y=3*x;
+//    Console.WriteLine("x="+x+" y="+y);
+//    x++;
+//} while (x<11);
+
+
+//Zad3.
+//Napisz program, który za pomocą instrukcji for wyświetla liczby całkowite od 1 do 20.
+
+//for (int x = 0; x <=20 ; x++)
+//{
+//	if (x<20)
+//	{
+//		Console.Write(x+",");
+//	}
+//	else
+//	{
+//		Console.Write(x+"."); 
+//	}
+//}
+
+//Zad4.
+//Napisz program, który za pomocą instrukcji do ... while wyświetla liczby całkowite od 1 do 20
+
+//int i = 1;
+
+//do
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+//while (i <= 20);
+
+//zad5
+//Napisz program, który za pomocą instrukcji while wyświetla liczby całkowite od 1 do 20.
+
+//int x = 1;
+
+//while (x <= 20)
+//{
+//    Console.WriteLine(x);
+//    x++;
+//}
+
+//Zad6.
+//Napisz program, który za pomocą instrukcji for sumuje liczby całkowite od 1 do 100.
+//int sum = 0;
+
+//for (int i = 1; i <= 100; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine("Suma liczb całkowitych od 1 do 100 wynosi: " + sum);
+
+//Zad7.
+//Napisz program, który za pomocą instrukcji do ... while sumuje liczby całkowite od 1 do 100.
+
+//int sum = 0;
+//int i = 1;
+//do
+//{
+//    sum += i;
+//    i++;
+//}
+//while (i <= 100);
+//Console.WriteLine("Suma liczb całkowitych od 1 do 100 wynosi: " + sum);
+
+//Zad8.
+//Napisz program, który za pomocą instrukcji while sumuje liczby całkowite od 1 do 100.
+
+int sum = 0;
+int i = 1;
+do
+{
+    sum += i;
+    i++;
 }
+while (i <= 100);
+Console.WriteLine("Suma liczb całkowitych od 1 do 100 wynosi: " + sum);
